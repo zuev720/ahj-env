f4cfd5bd43cf55fa2677e5c9c44a9fd65d38fc5b
zuev720 <zuev720mail.ru>
2021-05-13 18:21:47 +0300

    last version

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,12 +4,9 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-    // Set the mode to development or production
     mode: 'development',
-    // Control how source maps are generated
     devtool: 'inline-source-map',
 
-    // Spin up a server for quick development
     devServer: {
         historyApiFallback: true,
         contentBase: path.resolve(__dirname, '/dist'),
@@ -19,7 +16,6 @@ module.exports = merge(common, {
     },
 
     plugins: [
-        // Only update what has changed on hot reload
         new webpack.HotModuleReplacementPlugin(),
     ],
 });
